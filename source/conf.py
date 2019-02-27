fc953be42229606358d377ead747f95010d68976
isabel-schwende <isabel@mobius.ml>
2019-02-27 09:13:59 +0100

    add favicon icon

--- source/conf.py
+++ source/conf.py
@@ -116,6 +116,10 @@ html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'mobius_on-premise'
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "data/manifest_icon.png"
 
 # -- Options for LaTeX output ------------------------------------------------
 
